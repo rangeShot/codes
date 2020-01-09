2341b35836d820580e7adb2c339a3cfea755baa9
Ranjeet Kumar <rs119574@gmail.com>
2020-01-09 16:45:16 +0530

    Add files via upload
    Implementation of singly linked list, nesting & detecting loop

---
+++ Others/list.js
@@ -0,0 +1,81 @@
+//User Input
+let size=5;
+let arr=[1,2,3,4,5];
+let connectLastAt=2;
+
+
+class Node{
+    constructor(data){
+        this.data=data,
+        this.next=null;
+    }
+    getData(){
+        return this.data;
+    }
+}
+
+// Disply Linked list
+function disp(tempHead){
+    console.log("Displaying Linked list");
+    let temp=tempHead;
+    while(temp!=null){
+        console.log(temp.data);
+        temp=temp.next;
+    }
+    console.log("Null");
+    console.log();
+}
+
+// detect loop
+function hasCycle(head){
+    let tempHead1=head;
+    let tempHead2=head;
+    let haveCycle=true;
+    while(tempHead2.next.next != null){
+        tempHead1=tempHead1.next;
+        tempHead2=tempHead2.next.next;
+        if(tempHead1 == tempHead2){
+            haveCycle=true;
+            break;
+        }
+    }
+    return haveCycle;
+}
+
+
+// creating linked list
+let head=new Node(arr[0]);
+let count=1;
+let tempNode=head;
+while(count<size){
+    let temp=new Node(arr[count]);
+    tempNode.next=temp;
+    tempNode=tempNode.next;
+    count++;
+}
+
+// displaying the original linked list
+console.log("This is the original list");
+disp(head);
+
+//  nesting the orignal list
+let nestedNode=head;
+let lastNode=head;
+count=1;
+while(lastNode.next != null){
+    lastNode=lastNode.next;
+    if(count==connectLastAt)nestedNode=lastNode;
+    count++;
+}
+lastNode.next=nestedNode;
+console.log("The list has been nested");
+
+// disp(head);
+
+// detecting cycle
+if(hasCycle(head)){
+    console.log("loop detected");
+}
+else {
+    console.log("loop not detected");
+}
